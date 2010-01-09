fc7943d76cdc3e90438ac845076d8ad7684942f7
jtsiomb <jtsiomb@299b8958-fcc2-11de-8b3f-73507e9fc6a9>
2010-01-09 01:59:20 +0000

    stereowrap initial import
    git-svn-id: http://stereowrap.googlecode.com/svn/trunk@2 299b8958-fcc2-11de-8b3f-73507e9fc6a9

---
+++ Makefile
@@ -0,0 +1,35 @@
+name = stereowrap
+linkname = lib$(name).so
+soname = $(linkname).1
+lib_so = $(soname).0
+bin = $(name)
+
+PREFIX=/usr/local
+src = $(wildcard src/*.c)
+obj = $(src:.c=.o)
+
+CC = gcc
+INSTALL = install
+CFLAGS = -Wall -fPIC -g -D_GNU_SOURCE
+
+$(lib_so): $(obj)
+	$(CC) -o $@ -shared -Wl,-soname,$(soname) $(obj) $(LDFLAGS)
+
+.PHONY: clean
+clean:
+	rm -f $(obj) $(lib_so)
+
+.PHONY: install
+install:
+	$(INSTALL) -d $(PREFIX)/bin
+	$(INSTALL) -d $(PREFIX)/lib
+	$(INSTALL) -m 644 $(lib_so) $(PREFIX)/lib/$(lib_so)
+	cd $(PREFIX)/lib; rm -f $(linkname); ln -s $(lib_so) $(linkname)
+	$(INSTALL) -m 755 $(bin) $(PREFIX)/bin/$(bin)
+
+.PHONY: uninstall
+uninstall:
+	rm -f $(PREFIX)/lib/$(soname)
+	rm -f $(PREFIX)/lib/$(lib_so)
+	rm -f $(PREFIX)/lib/$(linkname)
+	rm -f $(PREFIX)/bin/$(bin)
---
+++ src/stereo.c
@@ -0,0 +1,471 @@
+/*
+stereowrap - an OpenGL stereoscopic emulation layer.
+Copyright (C) 2010 John Tsiombikas <nuclear@member.fsf.org>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <dlfcn.h>
+#include <X11/Xlib.h>
+#include <GL/gl.h>
+#include <GL/glx.h>
+
+#ifndef GLX_VERSION_1_3
+typedef void GLXFBConfig;
+#endif
+
+enum {
+	CROSS,
+	REDBLUE,
+	REDCYAN,
+	COLORCODE
+};
+
+#define LEFT_TEX	rtex[swap_eyes ? 1 : 0]
+#define RIGHT_TEX	rtex[swap_eyes ? 0 : 1]
+#define USE_SDR		(glCreateProgramObjectARB != 0)
+
+static int init(void);
+static int init_textures(void);
+static int init_sdr(void);
+void glDrawBuffer(GLenum buf);
+void glXSwapBuffers(Display *dpy, GLXDrawable drawable);
+XVisualInfo *glXChooseVisual(Display *dpy, int scr, int *attr);
+GLXFBConfig *glXChooseFBConfig(Display *dpy, int scr, const int *attr, int *nitems);
+static void show_stereo_pair(void);
+static void quad(float x1, float y1, float x2, float y2);
+static unsigned int create_pixel_shader(const char *src);
+static void show_cross(void);
+static void show_redblue(void);
+static void show_redcyan(void);
+static void show_colorcode(void);
+
+static void (*draw_buffer)(GLenum);
+static void (*swap_buffers)(Display*, GLXDrawable);
+static XVisualInfo *(*choose_visual)(Display*, int, int*);
+static GLXFBConfig *(*choose_fbconfig)(Display*, int, const int*, int*);
+
+static int xsz, ysz;
+static unsigned int rtex[2];
+static int cur_buf = -1;
+static int swap_eyes;
+static int stereo_method;
+static int grey;
+
+#ifdef GL_ARB_shader_objects
+static PFNGLCREATEPROGRAMOBJECTARBPROC glCreateProgramObjectARB;
+static PFNGLCREATESHADEROBJECTARBPROC glCreateShaderObjectARB;
+static PFNGLDELETEOBJECTARBPROC glDeleteObjectARB;
+static PFNGLATTACHOBJECTARBPROC glAttachObjectARB;
+static PFNGLSHADERSOURCEARBPROC glShaderSourceARB;
+static PFNGLCOMPILESHADERARBPROC glCompileShaderARB;
+static PFNGLLINKPROGRAMARBPROC glLinkProgramARB;
+static PFNGLUSEPROGRAMOBJECTARBPROC glUseProgramObjectARB;
+static PFNGLGETOBJECTPARAMETERIVARBPROC glGetObjectParameterivARB;
+static PFNGLGETINFOLOGARBPROC glGetInfoLogARB;
+static PFNGLGETUNIFORMLOCATIONARBPROC glGetUniformLocationARB;
+static PFNGLUNIFORM1IARBPROC glUniform1iARB;
+
+#ifndef GL_VERSION_1_3
+static PFNGLACTIVETEXTUREARBPROC glActiveTextureARB;
+#endif
+
+#endif	/* GL_ARB_shader_objects */
+
+static struct {
+	int id;
+	const char *name;
+	int need_sdr;
+	void (*func)(void);
+} method[] = {
+	{CROSS, "cross", 0, show_cross},
+	{REDBLUE, "redblue", 0, show_redblue},
+	{REDCYAN, "redcyan", 0, show_redcyan},
+#ifndef NOCOLORCODE
+	{COLORCODE, "colorcode", 1, show_colorcode},
+#endif
+	{0, 0, 0, 0}
+};
+
+
+static int init(void)
+{
+	static int init_done;
+	char *env = 0;
+
+	if(init_done) return 0;
+
+	draw_buffer = dlsym(RTLD_NEXT, "glDrawBuffer");
+	swap_buffers = dlsym(RTLD_NEXT, "glXSwapBuffers");
+	choose_visual = dlsym(RTLD_NEXT, "glXChooseVisual");
+	choose_fbconfig = dlsym(RTLD_NEXT, "glXChooseFBConfig");
+
+	if(!draw_buffer || !swap_buffers || !choose_visual) {
+		fprintf(stderr, "failed to load GL/GLX functions\n");
+		return -1;
+	}
+	
+	init_sdr();
+
+	if(getenv("STEREO_SWAP")) {
+		swap_eyes = 1;
+	}
+
+	if(getenv("STEREO_GREY")) {
+		grey = 1;
+	}
+
+	if((env = getenv("STEREO_METHOD"))) {
+		int i;
+		for(i=0; method[i].name; i++) {
+			if(strcmp(env, method[i].name) == 0) {
+				/* only select a shader-based method if we have shaders */
+				if(!method[i].need_sdr || USE_SDR) {
+					stereo_method = i;
+					break;
+				}
+			}
+		}
+	}
+
+	init_done = 1;
+	return 0;
+}
+
+static int init_textures(void)
+{
+	if(rtex[0] && rtex[1]) {
+		int vp[4];
+
+		glGetIntegerv(GL_VIEWPORT, vp);
+		if(vp[2] != xsz || vp[3] != ysz) {
+			xsz = vp[2];
+			ysz = vp[3];
+
+			glBindTexture(GL_TEXTURE_2D, rtex[0]);
+			glTexImage2D(GL_TEXTURE_2D, 0, grey ? 1 : 3, xsz, ysz, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
+
+			glBindTexture(GL_TEXTURE_2D, rtex[1]);
+			glTexImage2D(GL_TEXTURE_2D, 0, grey ? 1 : 3, xsz, ysz, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
+		}
+	} else {
+		int i;
+		glGenTextures(2, rtex);
+		for(i=0; i<2; i++) {
+			glBindTexture(GL_TEXTURE_2D, rtex[i]);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		}
+	}
+
+	return 0;
+}
+
+#define get_proc(type, name) \
+	(type)glXGetProcAddressARB((unsigned char*)name)
+
+static int init_sdr(void)
+{
+#ifdef GL_ARB_shader_objects
+	if(glCreateShaderObjectARB) {
+		return 0;
+	}
+
+	glCreateProgramObjectARB = get_proc(PFNGLCREATEPROGRAMOBJECTARBPROC, "glCreateProgramObjectARB");
+	glCreateShaderObjectARB = get_proc(PFNGLCREATESHADEROBJECTARBPROC, "glCreateShaderObjectARB");
+	glDeleteObjectARB = get_proc(PFNGLDELETEOBJECTARBPROC, "glDeleteObjectARB");
+	glAttachObjectARB = get_proc(PFNGLATTACHOBJECTARBPROC, "glAttachObjectARB");
+	glShaderSourceARB = get_proc(PFNGLSHADERSOURCEARBPROC, "glShaderSourceARB");
+	glCompileShaderARB = get_proc(PFNGLCOMPILESHADERARBPROC, "glCompileShaderARB");
+	glLinkProgramARB = get_proc(PFNGLLINKPROGRAMARBPROC, "glLinkProgramARB");
+	glUseProgramObjectARB = get_proc(PFNGLUSEPROGRAMOBJECTARBPROC, "glUseProgramObjectARB");
+	glGetObjectParameterivARB = get_proc(PFNGLGETOBJECTPARAMETERIVARBPROC, "glGetObjectParameterivARB");
+	glGetInfoLogARB = get_proc(PFNGLGETINFOLOGARBPROC, "glGetInfoLogARB");
+	glGetUniformLocationARB = get_proc(PFNGLGETUNIFORMLOCATIONARBPROC, "glGetUniformLocationARB");
+	glUniform1iARB = get_proc(PFNGLUNIFORM1IARBPROC, "glUniform1iARB");
+
+#ifndef GL_VERSION_1_3
+	glActiveTextureARB = get_proc(PFNGLACTIVETEXTUREARBPROC, "glActiveTextureARB");
+#endif
+
+	if(!glCreateProgramObjectARB) {
+		return -1;
+	}
+#endif	/* GL_ARB_shader_objects */
+	return 0;
+}
+
+void glDrawBuffer(GLenum buf)
+{
+	int new_buf = buf - GL_BACK_LEFT;
+
+	if(new_buf == cur_buf) {
+		return;
+	}
+	if(init() == -1) {
+		return;
+	}
+	init_textures();
+
+	if(cur_buf != -1) {
+		glBindTexture(GL_TEXTURE_2D, rtex[cur_buf]);
+		glCopyTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 0, 0, xsz, ysz);
+	}
+	cur_buf = new_buf;
+}
+
+void glXSwapBuffers(Display *dpy, GLXDrawable drawable)
+{
+	if(init() == -1) {
+		return;
+	}
+	init_textures();
+
+	if(cur_buf != -1) {
+		glBindTexture(GL_TEXTURE_2D, rtex[cur_buf]);
+		glCopyTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 0, 0, xsz, ysz);
+
+		show_stereo_pair();
+	}
+	cur_buf = -1;
+
+	swap_buffers(dpy, drawable);
+}
+
+XVisualInfo *glXChooseVisual(Display *dpy, int scr, int *attr)
+{
+	int a, *dest, *src;
+
+	if(init() == -1) {
+		return 0;
+	}
+
+	dest = src = attr;
+	do {
+		a = *src++;
+		if(a != GLX_STEREO) {
+			*dest++ = a;
+		}
+	} while(a != None);
+
+	return choose_visual(dpy, scr, attr);
+}
+
+GLXFBConfig *glXChooseFBConfig(Display *dpy, int scr, const int *attr, int *nitems)
+{
+	int a, *dest, *src;
+
+	if(init() == -1 || !choose_fbconfig) {
+		return 0;
+	}
+
+	dest = src = attr;
+	do {
+		a = *src++;
+		if(a != GLX_STEREO) {
+			*dest++ = a;
+		}
+	} while(a != None);
+
+	return choose_fbconfig(dpy, scr, attr, nitems);
+}
+
+
+static void show_stereo_pair(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glPushAttrib(GL_ALL_ATTRIB_BITS);
+
+	glMatrixMode(GL_PROJECTION);
+	glPushMatrix();
+	glLoadIdentity();
+	glMatrixMode(GL_TEXTURE);
+	glPushMatrix();
+	glLoadIdentity();
+	glMatrixMode(GL_MODELVIEW);
+	glPushMatrix();
+	glLoadIdentity();
+
+	glEnable(GL_TEXTURE_2D);
+	glDisable(GL_LIGHTING);
+	glDisable(GL_DEPTH_TEST);
+	glDisable(GL_STENCIL_TEST);
+	glDisable(GL_ALPHA_TEST);
+	glDisable(GL_BLEND);
+	glDisable(GL_FOG);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	glColor3f(1, 1, 1);
+
+	method[stereo_method].func();
+
+	glMatrixMode(GL_PROJECTION);
+	glPopMatrix();
+	glMatrixMode(GL_TEXTURE);
+	glPopMatrix();
+	glMatrixMode(GL_MODELVIEW);
+	glPopMatrix();
+
+	glPopAttrib();
+}
+
+static void show_cross(void)
+{
+	glBindTexture(GL_TEXTURE_2D, LEFT_TEX);
+	quad(-1, -1, 0, 1);
+	glBindTexture(GL_TEXTURE_2D, RIGHT_TEX);
+	quad(0, -1, 1, 1);
+}
+
+static void show_redblue(void)
+{
+}
+
+static void show_redcyan(void)
+{
+	glEnable(GL_TEXTURE_2D);
+
+	glBindTexture(GL_TEXTURE_2D, LEFT_TEX);
+	glColorMask(1, 0, 0, 1);
+	quad(-1, -1, 1, 1);
+	
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_ONE, GL_ONE);
+
+	glBindTexture(GL_TEXTURE_2D, RIGHT_TEX);
+	glColorMask(0, 1, 1, 1);
+	quad(-1, -1, 1, 1);
+
+	glColorMask(1, 1, 1, 1);
+}
+
+static void show_colorcode(void)
+{
+#ifdef GL_ARB_shader_objects
+	static int loc_left, loc_right;
+	static unsigned int sdr;
+	static const char *sdrsrc =
+		"uniform sampler2D left_tex, right_tex;\n"
+		"void main()\n"
+		"{\n"
+		"    vec3 left = texture2D(left_tex, gl_TexCoord[0].st).rgb;\n"
+		"    vec3 right = texture2D(right_tex, gl_TexCoord[0].st).rgb;\n"
+		"    vec3 col, coeff = vec3(0.15, 0.15, 0.7);\n"
+		"    col.r = left.r; col.g = left.g;\n"
+		"    col.b = dot(right, coeff);\n"
+		"    gl_FragColor = vec4(col, 1.0);\n"
+		"}\n";
+
+	if(!sdr) {
+		if((sdr = create_pixel_shader(sdrsrc))) {
+			loc_left = glGetUniformLocationARB(sdr, "left_tex");
+			loc_right = glGetUniformLocationARB(sdr, "right_tex");
+		} else {
+			return;
+		}
+	}
+
+	glUseProgramObjectARB(sdr);
+	glUniform1iARB(loc_left, 0);
+	glUniform1iARB(loc_right, 1);
+
+	glActiveTextureARB(GL_TEXTURE1_ARB);
+	glBindTexture(GL_TEXTURE_2D, RIGHT_TEX);
+	glEnable(GL_TEXTURE_2D);
+
+	glActiveTextureARB(GL_TEXTURE0_ARB);
+	glBindTexture(GL_TEXTURE_2D, LEFT_TEX);
+	glEnable(GL_TEXTURE_2D);
+
+	quad(-1, -1, 1, 1);
+
+	glActiveTextureARB(GL_TEXTURE1_ARB);
+	glDisable(GL_TEXTURE_2D);
+
+	glActiveTextureARB(GL_TEXTURE0_ARB);
+	glDisable(GL_TEXTURE_2D);
+
+	glUseProgramObjectARB(0);
+#endif	/* GL_ARB_shader_objects */
+}
+
+static void quad(float x1, float y1, float x2, float y2)
+{
+	glBegin(GL_QUADS);
+	glTexCoord2f(0, 0); glVertex2f(x1, y1);
+	glTexCoord2f(1, 0); glVertex2f(x2, y1);
+	glTexCoord2f(1, 1); glVertex2f(x2, y2);
+	glTexCoord2f(0, 1); glVertex2f(x1, y2);
+	glEnd();
+}
+
+static unsigned int create_pixel_shader(const char *src)
+{
+	unsigned int sdr, prog = 0;
+#ifdef GL_ARB_shader_objects
+	int success, info_len;
+	char *info_str = 0;
+	GLenum err;
+
+	glGetError();
+
+	sdr = glCreateShaderObjectARB(GL_FRAGMENT_SHADER_ARB);
+	glShaderSourceARB(sdr, 1, &src, 0);
+	glCompileShaderARB(sdr);
+
+	glGetObjectParameterivARB(sdr, GL_OBJECT_COMPILE_STATUS_ARB, &success);
+	glGetObjectParameterivARB(sdr, GL_OBJECT_INFO_LOG_LENGTH_ARB, &info_len);
+
+	if(info_len) {
+		if((info_str = malloc(info_len + 1))) {
+			glGetInfoLogARB(sdr, info_len, 0, info_str);
+			fprintf(stderr, "shader compilation: %s\n", info_str);
+			free(info_str);
+		}
+	}
+	if(!success) {
+		glDeleteObjectARB(sdr);
+		return 0;
+	}
+
+	prog = glCreateProgramObjectARB();
+	glAttachObjectARB(prog, sdr);
+	glLinkProgramARB(prog);
+
+	info_str = 0;
+	glGetObjectParameterivARB(prog, GL_OBJECT_LINK_STATUS_ARB, &success);
+	glGetObjectParameterivARB(prog, GL_OBJECT_INFO_LOG_LENGTH_ARB, &info_len);
+
+	if(info_len) {
+		if((info_str = malloc(info_len + 1))) {
+			glGetInfoLogARB(prog, info_len, 0, info_str);
+			fprintf(stderr, "shader linking: %s\n", info_str);
+			free(info_str);
+		}
+	}
+	if(!success) {
+		glDeleteObjectARB(sdr);
+		glDeleteObjectARB(prog);
+		return 0;
+	}
+
+	assert(glGetError() == GL_NO_ERROR);
+#endif	/* GL_ARB_shader_objects */
+	return prog;
+}
