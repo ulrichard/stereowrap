ddfb24865af6fb7cd2fc36e21ee51aaa7851fed0
Richard Ulrich <Richard.Ulrich@cubx-software.com>
2012-09-26 01:04:19 +0200

    pin assignment

--- arduino_tiny/ShutterGlasses/ShutterGlasses.ino
+++ arduino_tiny/ShutterGlasses/ShutterGlasses.ino
@@ -1,9 +1,18 @@
 
 #include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(5, 6); // receivePin, transmitPin
+// ATMEL ATTINY45 / ARDUINO
+//
+//                  +-\/-+
+// Ain0 (D 5) PB5  1|    |8  Vcc
+// Ain3 (D 3) PB3  2|    |7  PB2 (D 2)  Ain1
+// Ain2 (D 4) PB4  3|    |6  PB1 (D 1) pwm1
+//            GND  4|    |5  PB0 (D 0) pwm0
+//                  +----+
 
-const int leftEye   =  3;  
+SoftwareSerial mySerial(0, 1); // receivePin D0 pin5, transmitPin D1 pin6
+
+const int leftEye   =  4; // D4 pin3  
 int inByte = 0;
 
 void setup()   
